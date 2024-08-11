c9056750f79dc04dd5004dff54f9e2740374c7b3
HenriMatheus <tiaofodaxvideos@gmail.com>
2024-08-11 18:08:46 -0300

    correcao toobusy

--- src/middleware/tooBusy.js
+++ src/middleware/tooBusy.js
@@ -1,7 +1,7 @@
 const toobusy = require("toobusy-js")
 
 function tooBusyCheck(req, res, next) {
-    toobusy() ? res.status(503): next()
+    toobusy() ? res.status(503).send("Servidor ocupado."): next()
 }
 
 module.exports = tooBusyCheck
